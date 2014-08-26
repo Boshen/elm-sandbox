2ce72f590fa9a99c53d50f23beaf1d06a52983df
Boshen Chen <boshenc@gmail.com>
2014-08-26 21:59:58 +1200

    a moving arm

--- app/scripts/elm/Test.elm
+++ app/scripts/elm/Test.elm
@@ -3,22 +3,30 @@ module Test where
 import Keyboard
 import Window
 
-ball : { x:Float, y:Float }
-ball = { x=0, y=0 }
+ball : { x:Float, y:Float, angle: Float}
+ball = { x=0, y=0, angle=90}
 
 step (t, {x, y}) ball =
   let x' = toFloat x
       y' = toFloat y
+      angle' = ball.angle + y'
   in { ball | x <- ball.x + x'
-            , y <- ball.y + y' }
+            , angle <- if | angle' <= 0 -> 0 
+                          | angle' >= 180 -> 180
+                          | otherwise -> angle'
+      }
 
 delta = inSeconds <~ fps 25
 
 input = sampleOn delta (lift2 (,) delta Keyboard.arrows)
 
-display (w, h) {x, y} = collage w h
-  [ move (toFloat w/4, toFloat h/4) <| toForm (asText [x, y])
-  , move (x, y) <| filled black (circle 5)
-  ]
+display (w, h) {x, y, angle} = 
+  let dx = 10*cos(degrees (180-angle))
+      dy = 10*sin(degrees (180-angle))
+  in collage w h
+    [ move (toFloat w/4, toFloat h/4) <| toForm (asText [x, y, angle])
+    , move (x, y) <| filled black (rect 20 5)
+    , move (x+dx, y+dy) <| rotate (degrees (90-angle)) <| filled black (rect 2 20)
+    ]
 
 main = lift2 display Window.dimensions (foldp step ball input)
