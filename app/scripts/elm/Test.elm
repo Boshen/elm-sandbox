0b57c7bb0c444a52c655999759295466bb0946f2
Boshen Chen <boshenc@gmail.com>
2014-08-26 23:46:35 +1200

    gravity

--- app/scripts/elm/Test.elm
+++ app/scripts/elm/Test.elm
@@ -3,9 +3,9 @@ module Test where
 import Keyboard
 import Window
 
-type Bullet = { x:Float, y:Float, mv:Bool}
+type Bullet = { x:Float, y:Float, vx: Float, vy: Float, mv:Bool}
 bullet : Bullet
-bullet = { x=0, y=0, mv=False}
+bullet = { x=0, y=0, vx=0, vy=0, mv=False}
 
 tank : { x:Float, y:Float, angle:Float, bullet:Bullet}
 tank = { x=0, y=0, angle=90, bullet=bullet }
@@ -18,20 +18,22 @@ stepTank {x, y} tank =
             , angle <-  max 0 <| min angle' 180
       }
 
-stepBullet shoot tank =
+stepBullet t shoot tank =
   let dx = cos (degrees (180-tank.angle))
-      dy = sin (degrees tank.angle)
+      dy = sin (degrees tank.angle) + t*tank.bullet.vy
   in
     { tank | bullet <- if tank.bullet.mv
-                       then { bullet | x <- tank.bullet.x + dx
-                                     , y <- tank.bullet.y + dy
+                       then { bullet | x <- tank.bullet.x + 3*dx
+                                     , y <- max 0 tank.bullet.y + 3*dy
+                                     , vx <- tank.bullet.vx - t/2
+                                     , vy <- tank.bullet.vy - 8*t
                                      , mv <- True
                             }
                        else { bullet | mv <- shoot || tank.bullet.mv
                             }
     }
 
-step (t, arrows, shoot) = stepTank arrows . stepBullet shoot
+step (t, arrows, shoot) = stepTank arrows . stepBullet t shoot
 
 delta = inSeconds <~ fps 25
 
@@ -42,6 +44,7 @@ display (w, h) {x, y, angle, bullet} =
       dy = 10*sin(degrees (180-angle))
   in collage w h
     [ move (toFloat w/4, toFloat h/4) <| toForm (asText [x, y, angle, bullet.x, bullet.y])
+    , move (toFloat w/4, toFloat h/4-30) <| toForm (asText [bullet.vx, bullet.vy])
     , move (x, y) <| filled black (rect 20 5)
     , move (x+dx, y+dy) <| rotate (degrees (90-angle)) <| filled black (rect 2 20)
     , move (bullet.x, bullet.y) <| filled red (circle 3)
