392c83a087d1ac148e994bdd3d5c1d9ed536709f
Boshen Chen <boshenc@gmail.com>
2014-08-29 23:15:20 +1200

    move spaceship

--- app/scripts/elm/Game.elm
+++ app/scripts/elm/Game.elm
@@ -4,7 +4,7 @@ import Keyboard
 import Window
 
 -- model
-type Input = { x:Int, y:Int, space:Bool, delta: Time }
+type Input = { dx:Int, dy:Int, shoot:Bool, delta: Time, window: (Int, Int)}
 type Spaceship = { x:Float, y:Float }
 type Game = { spaceship: Spaceship }
 
@@ -18,19 +18,32 @@ defaultSpaceship = { x=0, y=0 }
 
 -- update
 stepGame : Input -> Game -> Game
-stepGame input game = game
+stepGame {dx, dy, shoot, delta, window} ({spaceship} as game) =
+  let spaceship' = updateSpaceship (dx, dy) window spaceship
+  in { game | spaceship <- spaceship'
+     }
+
+updateSpaceship : (Int, Int) -> (Int, Int) -> Spaceship -> Spaceship
+updateSpaceship (dx, dy) (w, h) spaceship =
+  let dx' = toFloat dx*2
+      dy' = toFloat dy*2
+      w' = toFloat w/2-10
+      h' = toFloat h/2-10
+  in { spaceship | x <- clamp -w' w' (spaceship.x + dx')
+                 , y <- clamp -h' h' (spaceship.y + dy')
+     }
 
 -- display
 display: (Int, Int) -> Game -> Input -> Element
-display (w, h) game input =
+display (w, h) ({spaceship} as game) input =
   collage w h
   [ debug (w,h) game input
-  , drawSpaceship game
+  , drawSpaceship spaceship
   ]
 
-drawSpaceship : Game -> Form
-drawSpaceship game =
-  ngon 3 10 |> filled black |> rotate (pi/2)
+drawSpaceship : Spaceship -> Form
+drawSpaceship {x, y} =
+  ngon 3 10 |> filled black |> rotate (pi/2) |> move (x, y)
 
 debug : (Int, Int) -> Game -> Input -> Form
 debug (w, h) game input =
@@ -38,15 +51,17 @@ debug (w, h) game input =
       y = toFloat h/4
   in group
   [ toForm (asText [game.spaceship.x, game.spaceship.y]) |> move (x, y)
-  , toForm (asText [input.x, input.y]) |> move (x, y+20)
+  , toForm (asText [input.dx, input.dy]) |> move (x, y+20)
+  , toForm (asText [fst input.window, snd input.window]) |> move (x, y+40)
   ]
 
 -- signals
-delta = inSeconds <~ fps 25
+delta = inSeconds <~ fps 60
 input = sampleOn delta <| Input <~ lift .x Keyboard.arrows
                                  ~ lift .y Keyboard.arrows
                                  ~ Keyboard.space
                                  ~ delta
+                                 ~ Window.dimensions
 
 -- main
 gameState : Signal Game
