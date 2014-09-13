5634fe16af652eeda432463f2a7bc74d0c259666
Boshen Chen <boshenc@gmail.com>
2014-09-14 00:22:48 +1200

    [snake] eat food and grow

--- app/scripts/snake/Snake.elm
+++ app/scripts/snake/Snake.elm
@@ -1,6 +1,7 @@
 module Snake where
 
 import Keyboard
+import Random
 import Window
 
 port title : String
@@ -14,7 +15,8 @@ type Game = { snake:Snake, food:Food }
 
 type Input = { dx:Int
              , dy:Int
-             , window:(Int, Int) }
+             , window:(Int, Int)
+             , rand:(Float, Float)}
 
 gridSize = 10
 
@@ -27,8 +29,14 @@ defaultSnake = { x=0, y=0, vx=0, vy=0, body=defaultBody }
 defaultBody = map (\x -> {x=x,y=0}) [0..10]
 
 stepGame : Input -> Game -> Game
-stepGame {dx, dy, window} ({snake} as game) =
-  { game | snake <- stepSnake (dx, dy) window snake }
+stepGame {dx, dy, window, rand} ({snake, food} as game) =
+  let eaten = snake.x == food.x && snake.y == food.y
+      snake' = (stepSnake (dx, dy) window) . (eatFood eaten food) <| snake
+      food' = updateFood eaten window rand food
+  in
+    { game | snake <- snake'
+           , food <- food'
+    }
 
 stepSnake : (Int, Int) -> (Int, Int) -> Snake -> Snake
 stepSnake (dx, dy) (w, h) ({x, y, vx, vy, body} as snake) =
@@ -41,6 +49,23 @@ stepSnake (dx, dy) (w, h) ({x, y, vx, vy, body} as snake) =
           , vy <- vy'
           , body <- {x=x,y=y} :: take ((length body)-1) body
      }
+
+eatFood : Bool -> Food -> Snake -> Snake
+eatFood eaten food snake =
+  if eaten then
+     {snake | body <- food :: snake.body}
+  else
+     snake
+
+updateFood : Bool -> (Int, Int) -> (Float, Float) -> Food -> Food
+updateFood eaten (w, h) (rx, ry) food =
+  if eaten then
+     {food | x <- round <| (rx-0.5)*(toFloat w)
+           , y <- round <| (ry-0.5)*(toFloat h)
+     }
+  else
+    food
+
 
 display : (Int, Int) -> Game -> Element
 display (w, h) {snake, food} =
@@ -55,18 +80,21 @@ draw clr {x, y} = move (toFloat x*gridSize, toFloat y*gridSize)
                       <| filled clr
                       <| square gridSize
 
-debug (w, h) {x, y, vx, vy} =
-  group [ move (toFloat w/4, toFloat h/4-20) <| toForm <| asText (w, h)
+debug (w, h) {x, y, vx, vy, body} =
+  group [ move (toFloat w/4, toFloat h/4-20) <| toForm <| asText <| length body
         , move (toFloat w/4, toFloat h/4) <| toForm <| asText (x, y, vx, vy)
         ]
 
 dt : Signal Time
-dt = fps 60
+dt = fps 30
+
+randWH = lift2 (,) (Random.float dt) (Random.float dt)
 
 input : Signal Input
 input = sampleOn dt <| Input <~ lift .x Keyboard.arrows
                               ~ lift .y Keyboard.arrows
                               ~ lift (\(w, h) -> ((div w gridSize), (div h gridSize))) Window.dimensions
+                              ~ randWH
 
 gameState : Signal Game
 gameState = foldp stepGame defaultGame input
