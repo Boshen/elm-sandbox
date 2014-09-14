fcc8656276b5cd5c809a38210f2548c1d60ae33d
Boshen Chen <boshenc@gmail.com>
2014-09-14 18:10:25 +1200

    [snake] improve code readability

--- app/scripts/snake/Snake.elm
+++ app/scripts/snake/Snake.elm
@@ -23,10 +23,10 @@ type Input = { dx:Int
 gridSize = 10
 
 defaultGame : Game
-defaultGame = { snake=defaultSnake, food={x=10, y=10}, state=Play}
+defaultGame = { snake=defaultSnake, food={x=10, y=10}, state=Play }
 
 defaultSnake : Snake
-defaultSnake = { x=1, y=0, vx=1, vy=0, body=(repeat 10 {x=0,y=0})}
+defaultSnake = { x=1, y=0, vx=1, vy=0, body=(repeat 10 {x=0,y=0}) }
 
 -- updates
 stepGame : Input -> Game -> Game
@@ -36,7 +36,7 @@ stepGame {dx, dy, window, restart, rand} ({snake, food, state} as game) =
       let eaten = snake.x == food.x && snake.y == food.y
           snake' = (stepSnake (dx, dy) window) . (eatFood eaten food) <| snake
           food' = updateFood eaten window rand food
-          state' = updateState window snake'
+          state' = updateState window snake
       in
         { game | snake <- snake'
                , food <- food'
@@ -47,48 +47,46 @@ stepGame {dx, dy, window, restart, rand} ({snake, food, state} as game) =
 
 stepSnake : (Int, Int) -> (Int, Int) -> Snake -> Snake
 stepSnake (dx, dy) (w, h) ({x, y, vx, vy, body} as snake) =
-  let (vx', vy') = if | dx /= 0 && vx == 0 -> (dx, 0)
-                      | dy /= 0 && vy == 0 -> (0, dy)
+  let (vx', vy') = if | dx /= 0 && vx == 0 -> (dx,  0)
+                      | dy /= 0 && vy == 0 -> ( 0, dy)
                       | otherwise          -> (vx, vy)
   in { snake | x <- clamp (div -w 2) (div w 2) (x + vx)
-          , y <- clamp (div -h 2) (div h 2) (y + vy)
-          , vx <- vx'
-          , vy <- vy'
-          , body <- {x=x,y=y} :: take ((length body)-1) body
+             , y <- clamp (div -h 2) (div h 2) (y + vy)
+             , vx <- vx'
+             , vy <- vy'
+             , body <- {x=x, y=y} :: take ((length body)-1) body
      }
 
 eatFood : Bool -> Food -> Snake -> Snake
 eatFood eaten food snake =
-  if eaten then
-     {snake | body <- food :: snake.body}
-  else snake
+  if eaten then {snake | body <- food :: snake.body} else snake
 
 updateFood : Bool -> (Int, Int) -> (Float, Float) -> Food -> Food
 updateFood eaten (w, h) (rx, ry) food =
   if eaten then
-     {food | x <- round <| (rx-0.5)*(toFloat w)
-           , y <- round <| (ry-0.5)*(toFloat h)
+     { food | x <- round <| (rx-0.5)*(toFloat w)
+            , y <- round <| (ry-0.5)*(toFloat h)
      }
   else food
 
 updateState : (Int, Int) -> Snake -> State
 updateState (w, h) {x, y, body} =
   if | abs x == div w 2 || abs y == div h 2 -> Over
-     | any (\c-> x == c.x && y == c.y) body -> Over
+     | any ((==) {x=x, y=y}) body -> Over
      | otherwise -> Play
 
 -- display
 display : (Int, Int) -> Game -> Element
-display (w, h) {snake, food, state} =
-  collage w h [ draw charcoal snake
+display (w, h) {snake, food} =
+  collage w h [ draw black snake
               , draw red food
-              , group <| map (draw darkGray) snake.body
+              , group <| map (draw black) snake.body
               ]
 
 draw : Color -> Position r -> Form
 draw clr {x, y} = move (toFloat x*gridSize, toFloat y*gridSize)
-                      <| filled clr
-                      <| square gridSize
+               <| filled clr
+               <| square gridSize
 
 -- signals
 dt : Signal Time
