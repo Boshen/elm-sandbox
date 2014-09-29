b38689ee0af19f612cbf58fb0322a0cac83b401c
Boshen Chen <boshenc@gmail.com>
2014-09-29 19:12:16 +1300

    [fractal] a less memory intensive version

--- app/scripts/fractal/Fractal.elm
+++ app/scripts/fractal/Fractal.elm
@@ -1,57 +1,52 @@
 module Fractal where
 
-import Window
-
 iters = 20
-diameter = 2
-init = (0, 0)
+(wx, wy) = (200, 200)
+(dx, dy) = (4/toFloat wx, 4/toFloat wy)
+defaultGame = { s = [], pos = (0, -2) }
 
-
-iterate : (Float, Float) -> (Float, Float) -> Int -> (Int, Float)
 iterate (r, i) (cr, ci) n =
   let r' = r * r - i * i + cr
       i' = 2 * r * i + ci
       z = r' * r' + i' * i'
   in
-    if z <= 4 && n < iters then
-      iterate (r', i') (cr, ci) (n + 1)
-    else
-      (n, z)
+     if z <= 4 && n < iters then
+        iterate (r', i') (cr, ci) (n + 1)
+     else
+        (n, z)
 
-range : Float -> Float -> Int -> [Float]
 range start end n =
   let step = (end - start) / toFloat n
   in indexedMap (\i x -> x + step * (toFloat i)) (repeat n start)
 
 log = logBase 10
-
 c n z =
   let mu = (toFloat n) - log (log z) / log 2
       a = sin mu / 2 + 0.5
       b = cos mu / 2 + 0.5
-  in rgb (255) (round <| a * 255) (round <| b * 255)
+  in rgb (255) (round <| a * 255) 255
 
-cell : (Float, Float) -> (Float, Float) -> Form
-cell (i, j) (dx, dy) =
-  let (n, z) = iterate init (i, j) 0
+cell (x, y) (i, j) =
+  let (n, z) = iterate (0, 0) (i, j) 0
       clr = if n == iters then black else c n z
   in
-    move (dx * diameter, dy * diameter) <| filled clr (circle diameter)
+     move (x - wx / 2, y - wy / 2) <| filled clr <| circle 1
 
-draw : (Int, Int) -> [Form]
-draw (w, h) =
-  let is = range -2 2 (w//3)
-      js = range -2 2 (h//3)
-  in indexedMap (\dx i ->
-    group <| indexedMap (\dy j ->
-      cell (i, j) (toFloat dx, toFloat dy)
-    ) js
-  ) is
+stepGame _ ({s, pos} as game) =
+   if fst pos == wx then
+      game
+   else
+    let
+        i' = fst pos + 1
+    in
+      { game | s <- s ++ indexedMap (\j' y -> cell (toFloat i', toFloat j') (snd pos, y)) (range -2 2 wy)
+             , pos <- (i', -2 + i' * dx)
+      }
 
-grid : (Int, Int) -> Element
-grid (w, h) =
-  collage w h
-  <| map (move (-(toFloat w)/4, -(toFloat h)/4))
-  <| draw (w, h)
+display {s} = collage wx wy s
 
-main = grid <~ Window.dimensions
+input = fps 60
+
+gameState = foldp stepGame defaultGame input
+
+main = display <~ gameState
